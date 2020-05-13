d8f2cf95f86442a206af326551361a37aa96305d
Pavel <pashmelkin@gmail.com>
2020-05-13 10:13:30 +1000

    Material ui (#3)
    * Add material UI

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import Button from '@material-ui/core/Button';
+
 import App from './App';
 
 test('renders learn react link', () => {
-  const { getByText } = render(<App />);
+  const { getByText } = render(<Button />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
