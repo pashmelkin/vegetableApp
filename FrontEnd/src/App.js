197ca94361ae3b1b90f44ef25c064f23de59ab84
Pavel Shmueli <Pavel.Shmueli@myob.com>
2020-07-07 16:10:28 +1000

    Add API call to backend

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Button from '@material-ui/core/Button';
 import './App.css';
+import ApiCall from './utils/ApiCall';
+
+
 
 function App() {
+
   return (
     <div>
-        <Button variant="contained" color="primary">
+        <Button variant="contained" color="primary" onClick={ApiCall}>
           Get Vegetables
         </Button>
     </div>
