6ecd6eede8fcba845aa749979966051b3e8ba208
Pavel Shmueli <Pavel.Shmueli@myob.com>
2020-07-08 17:27:16 +1000

    Introduce Typescript

--- FrontEnd/src/App.test.ts
+++ FrontEnd/src/App.test.ts
@@ -1,8 +1,5 @@
-import React from 'react';
 import { render } from '@testing-library/react';
 import Button from '@material-ui/core/Button';
-
-import App from './App';
 
 test('renders learn react link', () => {
   const { getByText } = render(<Button />);
