954f6d07e95e2e0c078e3d7bcc49ee2f6c64ee83
Pavel Shmueli <Pavel.Shmueli@myob.com>
2020-07-08 17:29:21 +1000

    Remove unit test

--- FrontEnd/src/App.test.ts
+++
@@ -1,8 +0,0 @@
-import { render } from '@testing-library/react';
-import Button from '@material-ui/core/Button';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<Button />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
