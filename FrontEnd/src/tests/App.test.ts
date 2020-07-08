1f046617715403a876b0d03d6d036780c2db809a
Pavel Shmueli <Pavel.Shmueli@myob.com>
2020-07-09 09:46:55 +1000

    Add initial test

---
+++ FrontEnd/src/tests/App.test.ts
@@ -0,0 +1,8 @@
+import App from '../App';
+
+describe('App', () => {
+        it('is as expected', () => {
+            const app = App();
+            expect(app).not.toBeNull();
+        });
+    });
