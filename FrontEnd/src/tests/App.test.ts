270cfab5020e510b0fe9f2b8dda62bf432ed5827
Pavel Shmueli <Pavel.Shmueli@myob.com>
2020-07-13 17:55:13 +1000

    Add typescript ujit test

--- FrontEnd/src/tests/App.test.ts
+++
@@ -1,8 +0,0 @@
-import App from '../App';
-
-describe('App', () => {
-        it('is as expected', () => {
-            const app = App();
-            expect(app).not.toBeNull();
-        });
-    });
