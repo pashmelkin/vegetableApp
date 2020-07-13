613d01782fe622abe2134cd804a2ed7bf959d499
Pavel Shmueli <Pavel.Shmueli@myob.com>
2020-07-14 09:47:34 +1000

    Fix materil ui unit test warning

--- FrontEnd/src/setupTests.js
+++ FrontEnd/src/setupTests.js
@@ -6,5 +6,8 @@ import '@testing-library/jest-dom/extend-expect';
 
 import { configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import React from "react"
 
 configure({ adapter: new Adapter() });
+
+React.useLayoutEffect = React.useEffect
