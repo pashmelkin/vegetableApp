a6abe0c02402c643104af6b9c1eadb69a0948b58
Pavel Shmueli <Pavel.Shmueli@myob.com>
2020-07-22 15:43:00 +1000

    Introduce the Grid

--- FrontEnd/src/VegetablesList.tsx
+++ FrontEnd/src/VegetablesList.tsx
@@ -2,6 +2,13 @@ import {ApiCallAsync} from './utils/ApiCallAsync';
 import React, {useState, useEffect} from "react";
 import config from './config.json';
 import {IVegetable} from './model/IVegetable';
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import Checkbox from "@material-ui/core/Checkbox";
+import {FormGroup} from "@material-ui/core";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import green from "@material-ui/core/colors/green";
+
 
 export  default function VegetablesList() {
     const [items, setItems] = useState<IVegetable[]>([]);
@@ -19,14 +26,26 @@ export  default function VegetablesList() {
 
 
         return (
-            <ul className="mdc-list mdc-list--two-line">
-                {items.map(item =>
-                    (
-                      <li key={item.name}>{item.name}</li>
+            <Grid container  spacing={2}>
+                <Grid item xs={12}>
+                    <Grid container justify="center" spacing={2}>
+                        {items.map((item) => (
+                            <Grid key={item.name} item>
+                                <Paper> {item.name}</Paper>
+                                <Paper> {(item ==== null) item.url?.match(/\d+$/)[0]}</Paper>
+                                <FormGroup row>
+                                    <FormControlLabel
+                                        control={<Checkbox name="checkedA" color="default"/>}
+                                        label="select"
+                                    />
+                                </FormGroup>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Grid>
+            </Grid>
 
-                    ))
-                }
-            </ul>
+
 
 
         );
